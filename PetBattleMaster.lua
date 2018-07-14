ed5160b52ed8bf094c35f6acd96705fdf95c0935
Daniel Smith <gtzippy@gmail.com>
2018-07-14 16:50:04 -0400

    adding petbattlemaster

---
+++ PetBattleMaster.lua
@@ -0,0 +1,111 @@
+
+PetBattleMasterDb = {
+	["namespaces"] = {
+		["PetTeamView"] = {
+			["profiles"] = {
+				["Default"] = {
+					["teams"] = {
+						{
+							["pets"] = {
+								{
+									["speciesId"] = 277,
+									["petType"] = 10,
+									["creatureId"] = 48609,
+									["abilities"] = {
+										384, -- [1]
+										713, -- [2]
+										710, -- [3]
+									},
+									["petId"] = "0x00000000028AE571",
+									["level"] = 25,
+									["name"] = "Clockwork Gnome",
+								}, -- [1]
+								{
+									["speciesId"] = 1152,
+									["petType"] = 2,
+									["creatureId"] = 68662,
+									["abilities"] = {
+										110, -- [1]
+										362, -- [2]
+										593, -- [3]
+									},
+									["petId"] = "0x00000000028AE62C",
+									["level"] = 25,
+									["name"] = "Chrominius",
+								}, -- [2]
+								{
+									["speciesId"] = 1183,
+									["petType"] = 10,
+									["creatureId"] = 69820,
+									["abilities"] = {
+										384, -- [1]
+										940, -- [2]
+										392, -- [3]
+									},
+									["petId"] = "0x00000000028AE64F",
+									["level"] = 25,
+									["name"] = "Son of Animus",
+								}, -- [3]
+							},
+							["name"] = "A Team",
+							["icon"] = "INTERFACE\\ICONS\\Ability_Ambush",
+						}, -- [1]
+					},
+				},
+			},
+		},
+	},
+	["profileKeys"] = {
+		["Tipsyox - Bladefist"] = "Default",
+		["Andradene - Moon Guard"] = "Default",
+		["Priestface - Kul Tiras"] = "Default",
+		["Zosimus - Bladefist"] = "Default",
+		["Migatres - Bladefist"] = "Default",
+		["Joopa - Bladefist"] = "Default",
+		["Lockboxes - Bladefist"] = "Default",
+		["Burdell - Bladefist"] = "Default",
+		["Icebloxrox - Kul Tiras"] = "Default",
+		["Teats - Bladefist"] = "Teats - Bladefist",
+		["Hardwoöd - Bladefist"] = "Default",
+		["Gynum - Bladefist"] = "Default",
+		["Darrila - Bladefist"] = "Default",
+		["Caity - Bladefist"] = "Default",
+		["Samakigg - Mal'Ganis"] = "Default",
+	},
+	["profiles"] = {
+		["Default"] = {
+			["frames"] = {
+				["PetJournalParent"] = {
+				},
+				["petbmTeamView"] = {
+					["windowPosition"] = {
+						["y"] = -113,
+						["x"] = 173,
+						["point"] = "TOP",
+						["relativePoint"] = "TOP",
+					},
+				},
+				["petbmNameIconEditor"] = {
+				},
+				["petbmInfoView"] = {
+				},
+			},
+		},
+		["Teats - Bladefist"] = {
+			["frames"] = {
+				["petbmTeamView"] = {
+					["windowPosition"] = {
+						["y"] = -23,
+						["x"] = -285,
+						["point"] = "TOP",
+						["relativePoint"] = "TOP",
+					},
+				},
+				["petbmInfoView"] = {
+				},
+				["petbmNameIconEditor"] = {
+				},
+			},
+		},
+	},
+}
